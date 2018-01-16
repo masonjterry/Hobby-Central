c10dfffe2ceba7e680fa6603962ccb5e33c9a4dc
chris-dion <michael.c.dion@gmail.com>
2018-01-16 16:32:40 -0600

    ajax test

--- public/assets/js/directions.js
+++ public/assets/js/directions.js
@@ -21,6 +21,15 @@ $.get("/api/directions", function(data){
       $("#instructions-div").text(data[i].instructions);
       $("#images-div").attr("src", data[i].images);
       $("#videos-div").attr("src", data[i].videos);
+
+      var queryURL = "http://api.walmartlabs.com/v1/search?query=hammer&format=json&apiKey=72q26xxyzrdrsjqt8ux7dgyj";
+      $.ajax({
+         url: queryURL,
+         method: "GET"
+       }).done(function(response) {
+         console.log(response);
+      });
+
       //split data[i].materials by ","
       //create a slick carousel
       //for loop through the new array
